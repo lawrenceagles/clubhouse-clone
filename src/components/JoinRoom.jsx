4f924cef3dfc5b29a35dcff731f7dd3f677b4274
lawrenceagles <lawrenceagles@gmail.com>
2021-10-16 10:06:05 +0100

    added room component

--- src/components/JoinRoom.jsx
+++ src/components/JoinRoom.jsx
@@ -4,42 +4,51 @@ import { useHMSActions } from '@100mslive/hms-video-react';
 function JoinForm() {
 	const hmsActions = useHMSActions();
 	const [ userName, setUserName ] = useState('');
-	const [ authToken, setAuthToken ] = useState('');
+	// const [ authToken, setAuthToken ] = useState('');
 
 	const handleSubmit = () => {
-		hmsActions.join({
-			userName,
-			authToken
-		});
+		console.log('sumit', userName);
+		hmsActions.join({ userName });
 	};
 
 	return (
-		<form>
-			<h2>Join Room</h2>
-			<div className="input-container">
-				<input
-					value={userName}
-					onChange={(e) => setUserName(e.target.value)}
-					id="username"
-					type="text"
-					name="username"
-					placeholder="Enter Username"
-				/>
+		<section className="bg-gray-200 relative min-h-screen antialiased border-t-8 border-black">
+			<div className="max-w-sm mx-auto px-6">
+				<div className="relative h-screen flex flex-wrap items-center">
+					<div className="w-full relative">
+						<div className="mt-6">
+							<div className="text-center font-semibold text-black text-2xl"> Join Room </div>
+
+							<form className="mt-8">
+								<div className="mx-auto max-w-lg ">
+									<div className="py-1">
+										<span className="px-1 text-sm text-gray-600">Username</span>
+										<input
+											onChange={(e) => setUserName(e.target.value)}
+											value={userName}
+											placeholder="Enter Username"
+											type="text"
+											className="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 
+         border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500
+         focus:bg-white focus:border-gray-600 focus:outline-none"
+										/>
+									</div>
+
+									<button
+										type="button"
+										onClick={handleSubmit}
+										className="mt-3 text-lg font-semibold bg-gray-800 w-full text-white 
+     rounded-lg px-6 py-3 block shadow-xl hover:text-white hover:bg-black"
+									>
+										Join
+									</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div className="input-container">
-				<input
-					value={authToken}
-					onChange={(e) => setAuthToken(e.target.value)}
-					id="authToken"
-					type="text"
-					name="authToken"
-					placeholder="Enter Auth token"
-				/>
-			</div>
-			<button onClick={handleSubmit} className="btn-primary">
-				Join
-			</button>
-		</form>
+		</section>
 	);
 }
 
