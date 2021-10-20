66e0c7734608fc5f32e9695442b60ac265aa3c6c
lawrenceagles <lawrenceagles@gmail.com>
2021-10-20 16:47:53 +0100

    added uuid

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import { Fragment, useEffect } from 'react';
-import { selectIsConnectedToRoom, useHMSActions, useHMSStore } from '@100mslive/hms-video-react';
+import { Fragment } from 'react';
+import { selectIsConnectedToRoom, useHMSStore } from '@100mslive/hms-video-react';
 import JoinForm from './components/JoinRoom';
 import Room from './components/Room';
 
 const App = () => {
 	const isConnected = useHMSStore(selectIsConnectedToRoom);
-	const hmsActions = useHMSActions();
-
-	useEffect(
-		() => {
-			window.onunload = () => {
-				if (isConnected) {
-					hmsActions.leave();
-				}
-			};
-		},
-		[ hmsActions, isConnected ]
-	);
 
 	return (
 		<div className="App">
